108556ba0905f1819892da0bb3d4503b81aad1b8
JayantUMDDev <jmaheshw@terpmail.umd.edu>
2026-04-28 17:34:31 -0400

    Fix: remove unused useRef import in TeacherView (ESLint CI error)

--- src/views/TeacherView.jsx
+++ src/views/TeacherView.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Slides from "../components/Slides";
 import NavigationBar from "../components/NavigationBar";
 import NotesSidebar from "../components/NotesSidebar";
